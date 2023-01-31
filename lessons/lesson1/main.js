a42add374ca113a3623785d9cb67fe463e241597
Оля <zembitsky.v@gmail.com>
2023-02-01 00:06:55 +0200

    половина

---
+++ lessons/lesson1/main.js
@@ -0,0 +1,20 @@
+console.log('Hello');
+console.log('Owu');
+console.log('com');
+console.log('ua');
+console.log('1');
+console.log('10');
+console.log('-999');
+console.log('123');
+console.log('3.14')
+console.log('2.7')
+console.log('16')
+
+
+
+let a = 100;
+console.log(typeof a);
+let b = 100;
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
