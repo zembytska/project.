b69016a8a69f774d666f38966ae2268fb12be20b
Оля <zembitsky.v@gmail.com>
2023-02-01 14:57:24 +0200

    new

--- lessons/lesson1/main.js
+++ lessons/lesson1/main.js
@@ -1,24 +1,3 @@
 
-console.log('hello');
-console.log('Owu');
-console.log('com');
-console.log('ua');
-console.log('1');
-console.log('10');
-console.log('-999');
-console.log('123');
-console.log('3.14');
-console.log('2.7');
-console.log('16');
-console.log('true');
-console.log('false');
-let str='Olga';
-let str2="Zembitska";
-console.log(str+str2);
 
-let a = 100;
-console.log(typeof a);
-let b = 100;
-console.log(typeof b);
-let c = true;
-console.log(typeof c);
+
