0fc475747d7fb4a8690933c5b18d2cb25966124c
Оля <zembitsky.v@gmail.com>
2023-02-04 23:32:08 +0200

    ready

--- lessons/lesson2/main.js
+++ lessons/lesson2/main.js
@@ -120,4 +120,35 @@ let y = undefined;
  y = y || 'default';
 console.log(y);
 // - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
-// Footer
+
+//let coursesAndDurationArray = [
+  ////  {title: 'JavaScript Complex', monthDuration: 5},
+    //{title: 'Java Complex', monthDuration: 6},
+    //{title: 'Python Complex', monthDuration: 6},
+    //{title: 'QA Complex', monthDuration: 4},
+   /// {title: 'FullStack', monthDuration: 7},
+    //{title: 'Frontend', monthDuration: 4}
+let JavaScript = 5;
+if (5===5){
+    console.log(false);
+}
+let Java = 6;
+if (6>5){
+    console.log('super');
+}
+let Python = 6;
+if (6>5){
+    console.log('super');
+}
+let QA = 4;
+if (5>4){
+    console.log(false);
+}
+let FullStack = 7;
+if (7>5){
+    console.log('super');
+}
+let Frontend = 4;
+if (4<5){
+    console.log(false);
+}
