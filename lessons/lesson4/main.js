aa176a2a956bb249f106d7bdbf938e8fabebad2a
Оля <zembitsky.v@gmail.com>
2023-02-11 23:35:18 +0200

    4

--- lessons/lesson4/main.js
+++ lessons/lesson4/main.js
@@ -71,8 +71,9 @@ function goo(description) {
 }
 goo('text');
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function create(description) {
-    document.write(
+function copy(howmanytimes,description) {
+    for (let i=0; i < howmanytimes; i++)
+   document.write(
  `
    <ul>
     <li>${description}</li>
@@ -83,42 +84,36 @@ function create(description) {
     );
 }
 
-create('menu');
+copy(2,'menu');
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 //- створити функцію яка повертає найменьше число з масиву
 
-        let users = [
-            {name: 'vasya', age: 31, status: false},
-            {name: 'petya', age: 30, status: true},
-            {name: 'kolya', age: 29, status: true},
-            {name: 'olya', age: 28, status: false},
-            {name: 'max', age: 30, status: true},
-            {name: 'anya', age: 30, status: false},
-            {name: 'oleg', age: 28, status: false},
-            {name: 'andrey', age: 29, status: true},
-            {name: 'masha', age: 30, status: true},
-            {name: 'olya', age: 23, status: false},
-            {name: 'max', age: 31, status: true}
-        ];
+        let nomers = [15, 6, 18, 22];
 
-        function userFilter(users) {
-            for (const user of users) {
-                if (user.age < 24) {
-                    console.log(user);
-                }
-            }
-            return userFilter;
-        }
 
- userFilter(users)
+console.log(nomers)
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+let sum = [1, 2, 10]
+function sum2(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+    console.log(sum2(1, 2, 10));
+
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-
+function swap(index1, index2, ...x){
+    console.log(x)
+}
+swap(11,22,22, 11, 33, 44)
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)}
-
-
-
-
+function exchange (sumUAH) {
+let currencyValues = 40 / 1;
+let exchangeCurrency = 38 * 1;
+let currency = sumUAH / currencyValues;
+let exchange = sumUAH * exchangeCurrency;
+return currency;
+}
+let number = exchange(200)
+console.log(number);
