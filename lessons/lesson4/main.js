bd3d8f80c96f89556ec8559ca6c3ac78410650e2
Оля <zembitsky.v@gmail.com>
2023-02-11 17:16:56 +0200

    3

--- lessons/lesson4/main.js
+++ lessons/lesson4/main.js
@@ -45,7 +45,7 @@ function printerArray (array) {
 }
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function writer (description, ) {
+function writer (description ) {
     document.write(
         `
         <div>
@@ -58,40 +58,67 @@ function writer (description, ) {
 writer('some fresh milk');
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-
-
+function goo(description) {
+    document.write(
+    `
+    <ul>
+      <li>${description}</li>
+      <li>${description}</li>
+      <li>${description}</li>
+    </ul>
+    `
+    );
+}
+goo('text');
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+function create(description) {
+    document.write(
+ `
+   <ul>
+    <li>${description}</li>
+    <li>${description}</li>
+    <li>${description}</li>
+    </ul>
+    `
+    );
+}
+
+create('menu');
+
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-// - створити функцію яка повертає найменьше число з масиву
 //- створити функцію яка повертає найменьше число з масиву
 
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 30, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 23, status: false},
-    {name: 'max', age: 31, status: true}
-];
-function userFilter(users) {
-    for (const user of users) {
-        if (user.age < 24) {
-            console.log(user);
+        let users = [
+            {name: 'vasya', age: 31, status: false},
+            {name: 'petya', age: 30, status: true},
+            {name: 'kolya', age: 29, status: true},
+            {name: 'olya', age: 28, status: false},
+            {name: 'max', age: 30, status: true},
+            {name: 'anya', age: 30, status: false},
+            {name: 'oleg', age: 28, status: false},
+            {name: 'andrey', age: 29, status: true},
+            {name: 'masha', age: 30, status: true},
+            {name: 'olya', age: 23, status: false},
+            {name: 'max', age: 31, status: true}
+        ];
+
+        function userFilter(users) {
+            for (const user of users) {
+                if (user.age < 24) {
+                    console.log(user);
+                }
+            }
+            return userFilter;
         }
-    }
-    return userFilter;
-}
- userFilter(users);
 
-
+ userFilter(users)
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
-// Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)}
+
+
+
+
