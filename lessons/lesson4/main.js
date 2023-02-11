063bc807739e20bf98de59f9fe63e673ac114c34
Оля <zembitsky.v@gmail.com>
2023-02-11 11:52:19 +0200

    3

--- lessons/lesson4/main.js
+++ lessons/lesson4/main.js
@@ -20,13 +20,46 @@ function cylinder ( pi, pi, r, r,h ){
     let result = (pi + pi) * r * (r+ h);
     return result ;
 }
-let r3 = cylinder(2, 3.14, 2,2, 4 );
+let r3 = cylinder(3.14, 3.14, 2,2, 4 );
 console.log(r3)
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
+let colors = [
+    {name: 'black', status: false},
+    {name: 'white', status: true},
+    {name: 'blue',  status: true},
+    {name: 'yellow',  status: false},
+    {name: 'pink',  status: true},
+    {name: 'braun',  status: false},
+    {name: 'grey',  status: false},
+    {name: 'red',  status: true},
+    {name: 'orange', status: true},
+    {name: 'green',  status: false},
+    {name: 'purple', status: true}
+];
+printerArray(colors);
+function printerArray (array) {
+    for (const item of array) {
+        console.log(item)
+    }
+}
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+function writer (description, ) {
+    document.write(
+        `
+        <div>
+            <h1>milk</h1>
+            <p>${description}</p>
+        </div>
+        `
+    );
+}
+writer('some fresh milk');
+
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
