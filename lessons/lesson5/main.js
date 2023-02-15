d6681fd9abf36e6b2240151bc0fb76ad907d16cf
Оля <zembitsky.v@gmail.com>
2023-02-15 14:18:56 +0200

    2

--- lessons/lesson5/main.js
+++ lessons/lesson5/main.js
@@ -11,7 +11,7 @@ console.log(cylin(3.14, 3.14, 2,2,4));
 
 //- створити функцію яка приймає масив та виводить кожен його елемент
 
-let printerArray = function (array) {
+let printerArray = (array) => {
     for (const item of array) {
         console.log(item)
     }
@@ -31,8 +31,9 @@ let colors = [
 ];
 printerArray(colors);
 
+
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-let paragraph = function (arguments) {
+ let paragraph = (arguments) => {
     document.write(
         `
         <div>
@@ -42,9 +43,9 @@ let paragraph = function (arguments) {
        `
     );
 }
-paragraph('milk')
+paragraph('some fresh milk')
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function goo(description) {
+let goo = (description)=> {
     document.write(
         `
     <ul>
@@ -58,26 +59,57 @@ function goo(description) {
 goo('java');
  // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
+let liCreator = (description, counter)=> {
+    document.write(`<ul>`);
+    for (let i = 0; i < counter; i++) {
+         document.write(`<li>${description}</li>`);
+     }
+     document.write(`</ul>`);
+}
 
-
-
+ liCreator('menu',3);
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+const numbers = [1, 2, 3, 'Hi', true];
+let too =  (item)=> {
+    document.write(`<ul>`)}
+    for (const item of numbers) {
+        document.write(`<li>${item}</li>`);
+    }
+{  document.write(`</ul>`)};
+
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+let objCreator = (objects)=> {
+    for (const item of objects) {
+        document.write(`<div>${item.id} ${item.name} ${item.age}</div>`);
+    }
+}
+let names = [
+    {
+        id: 'snow',
+        name: 'masha',
+        age: 22
+    }
+];
+objCreator(names);
+
+
 // - створити функцію яка повертає найменьше число з масиву
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 
 
-let sum2 = function sum2(numbers) {
-    let basket = 0;
-    for (const number of numbers) {
-        basket *= number;
+ let sum=(numberos)=> {
+    let basket = 0; {
+        for (const number of numberos)
+        basket = basket + number;
     }
-    return basket;
-}
 
-console.log(sum2(numbers));
-let sum = [1, 2, 10];
+
+ }
+let number = [1,10];
+sum(number)
+
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
