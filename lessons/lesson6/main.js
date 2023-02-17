2eaf83c8f618fe98d29c9024b3eebd58319aeeb1
Оля <zembitsky.v@gmail.com>
2023-02-17 20:40:49 +0200

    2

--- lessons/lesson6/main.js
+++ lessons/lesson6/main.js
@@ -1,32 +1,36 @@
 // - Знайти та вивести довижину настипних стрінгових значень
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
-let str = 'hello world';
-console.log(str);
-console.log(str.toLocaleLowerCase());
+let strin = 'hello world, lorem ipsum, javascript is cool';
+console.log(strin.toUpperCase());
+
 // - Перевести до нижнього регістру настипні стрінгові значення
-// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+ let string = 'HELLO WORLD, LOREM IPSUM, JAVASCRIPT IS COOL';
+ console.log(string.toLowerCase());
+
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-//
-//
+let stoo = ' dirty string   ';
+let split = stoo.replaceAll(' ','');
+        console.log( split);
 // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
-
 let stringToarray = (str)=> str.split(' ');
-
         console.log(stringToarray('ревуть воли як ясла повні'));
-
 let str = 'Ревуть воли як ясла повні';
 
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+let numbers = [10,8,-7,55,987,-1011,0,1050,0];
+
 
 
 // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-//     let nums = [11,21,3];
+  let nums = [11,21,3];
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
-//
-//
+
+let sort = nums.sort((a, b) => a-b);
+console.log(sort);
 // - є масив
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
